f08631c5132f56f2e43bb8e70bdcc87dba8e08a7
Fernando Marins <f.augustomarins@gmail.com>
2022-04-19 10:56:48 -0300

    first commit

---
+++ twitter-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-clone
+//
+//  Created by Fernando Marins on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
