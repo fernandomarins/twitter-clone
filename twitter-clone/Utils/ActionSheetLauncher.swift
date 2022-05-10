599d2ea0d93172e7f0b9ae43597c91d42c292197
Fernando Marins <f.augustomarins@gmail.com>
2022-05-10 09:20:29 -0300

    displaying the action sheet

--- twitter-clone/Utils/ActionSheetLauncher.swift
+++ twitter-clone/Utils/ActionSheetLauncher.swift
@@ -12,17 +12,92 @@ class ActionSheetLauncher: NSObject {
     // MARK: - Properties
     
     private let user: User
+    private let tableView = UITableView()
+    private let reuseIdentifier = "actionSheetCell"
+    
+    private var window: UIWindow?
+    
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        view.addGestureRecognizer(tap)
+        
+        return view
+    }()
     
     // MARK: - Lifecycle
     
     init(user: User) {
         self.user = user
         super.init()
+        configureTableView()
     }
     
     // MARK: - Helpers
     
     func show() {
         print("action sheet for \(user.userName)")
+        
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        guard let window = window else { return }
+//        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+        self.window = window
+        
+        window.addSubview(blackView)
+        blackView.frame = window.frame
+        
+        window.addSubview(tableView)
+        tableView.frame = CGRect(x: 0,
+                                 y: window.frame.height,
+                                 width: window.frame.width,
+                                 height: 300)
+        
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.blackView.alpha = 1
+            self?.tableView.frame.origin.y -= 300
+        }
+
     }
+    
+    private func configureTableView() {
+        tableView.backgroundColor = .red
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.isScrollEnabled = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: - Selectors
+    
+    @objc private func handleDismissal() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.blackView.alpha = 0
+            self?.tableView.frame.origin.y += 300
+        }
+    }
+}
+
+extension ActionSheetLauncher: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+}
+
+extension ActionSheetLauncher: UITableViewDelegate {
+    
 }
